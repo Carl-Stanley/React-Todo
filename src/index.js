ad4b8cb18849be14707db0f37c8bebc4912776bd
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-09-10 18:17:53 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
- 
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+      families: ['Titillium Web:300,400,700', 'sans-serif']
+    }
+  });
 
 render(<App />, document.getElementById('root'));
