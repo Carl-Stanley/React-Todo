e8c3d20754284b1158ba13ab091de1540cb01b14
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-09-10 19:21:13 -0700

    commit

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,7 +1,7 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
-import React from 'react'
-import Todo from './Todo'
+import React from 'react';
+import Todo from './Todo';
 
 const TodoList = props => {
   
@@ -33,4 +33,4 @@ const TodoList = props => {
   )
 }
 
-export default TodoList
+export default TodoList;
